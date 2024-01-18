fd7ad99e6debb8c20aaec47c1368a64fe7483148
EzekielCz <ezechielcazacu@gmail.com>
2024-01-18 02:56:22 -0600

    thymeleaf form event updates

--- demo/src/main/java/org/launchcode/demo/controllers/EventController.java
+++ demo/src/main/java/org/launchcode/demo/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +14,16 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List <String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("doomsDay");
-        events.add("zeta");
-        events.add("unsc");
-        events.add("stalker");
+//        List<String> events = new ArrayList<>();
+//        events.add("dooms Day");
+//        events.add("Zeta Ring");
+//        events.add("UNSC");
+//        events.add("Stalker");
+//        model.addAttribute("events", events);
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -26,5 +31,12 @@ public class EventController {
     @GetMapping("create")
     public String createEventForm(){
         return "events/create";
+    }
+
+    // will be at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
     }
 }
