67ae067c3a267ef40f152ab66f6accedf4736e06
EzekielCz <ezechielcazacu@gmail.com>
2024-01-18 02:38:00 -0600

    thymeleaf form

---
+++ demo/src/main/java/org/launchcode/demo/CodingeventsApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingeventsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CodingeventsApplication.class, args);
+	}
+
+}
